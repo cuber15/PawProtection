87258b97f5f5adfea23edacd148357ca36e82ffb
cuber15 <lcs2019053@iiitl.ac.in>
2021-09-02 23:13:41 +0530

    added files

---
+++ routes/login.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const indexRouter = express.Router();
+
+indexRouter.use(bodyParser.json());
+
+indexRouter.route('/')
+.all((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.render('frontPage');
+})
+
+module.exports = indexRouter;

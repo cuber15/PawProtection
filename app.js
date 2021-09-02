06e9dcbae5c5c7e68fdc1b799b37f63ef1388e36
stark210 <shristisingh0210@gmail.com>
2021-09-02 22:27:28 +0530

    Basic server setup

---
+++ app.js
@@ -0,0 +1,24 @@
+//server using express...
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = 8080;
+const hostname = '127.0.0.1';
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.all('/', (req, res, next) => {
+    res.statusCode = 200;
+    res.send("hello");
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
